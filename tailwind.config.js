af9dba96cd48bd485a0081b61d337258897a1991
wtrm3ln <142867065+wtrm3ln@users.noreply.github.com>
2024-09-10 20:28:44 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#238757',
+        light: '#BFBFBF'
       },
     },
   },
