238619ad9956f2d50835339588af837047608932
wtrm3ln <142867065+wtrm3ln@users.noreply.github.com>
2024-09-11 15:48:10 +0530

    h

--- app/page.js
+++ app/page.js
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen grid justify-center items-center">
+    <div className="h-screen grid gap-5 md:pl-48 pt-12">
 
-      <div className="hidden md:flex fixed rounded-full bg-primary bottom-0 md:top-0 md:-left-[70vh] h-[90vh] w-[90vh] flex items-center justify-center rounded-full">
-          <div className="rounded-full w-[90%] h-[90%] border-2 border-light flex items-center justify-center">
+      <div className="hidden md:flex fixed rounded-full bg-primary -bottom-[60vh] -left-[110vh] h-[150vh] w-[150vh] flex items-center justify-center rounded-full">
+          <div className="rounded-full w-[90%] h-[90%] border-2 border-light flex items-center justify-center shadow-xl ">
           <div className="rounded-full w-[97%] h-[97%] border-dashed border-2 border-light">
             
             </div>
           </div>
         </div>
 
-        <div className="fixed top-0 right-0 p-4 flex gap-6 flex-wrap items-center justify-center">
+        <div className="fixed z-10 top-0 bottom-0 flex flex-col justify-between right-0 p-4 text-primary">
+          <div>
+          <p className="font-bold">Contact Us</p>
             <a
               className="flex items-center gap-2 hover:underline hover:underline-offset-4"
               href="/"
@@ -30,20 +32,40 @@ export default function Home() {
             >
               +91 9499116400 
             </a>
-        </div>
+            </div>
+            <div>
+            <p className="font-bold text-black">Visit our parent website:</p>
+            <a
+              className="flex items-center gap-2 text-black hover:underline hover:underline-offset-4"
+              href="https://tulsitechlabs.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                aria-hidden
+                src="https://nextjs.org/icons/globe.svg"
+                alt="Globe icon"
+                width={16}
+                height={16}
+              />
+              Tulsi Tech Labs Pvt Ltd→
+            </a>
+            </div>
+      </div>
 
-      <main className="mx-auto my-36 md:my-auto md:flex py-14 px-6 md:pl-48">
+      <div className="p-10">
+      <main className="mx-auto md:flex justify-bottom px-6">
           
           <Image
           src="/yaatrip-1.png"
           alt="Yaatrip"
           width={700} // Adjust width as needed
-          height={100}
-          style={{ objectFit: 'contain' }}
+          height={400}
+          style={{ objectFit: 'contain', objectPosition: 'bottom' }}
           />
         
 
-        <div className="-ml-10 relative rounded-full flex items-center justify-center rounded-full">
+        <div className="-ml-10 relative rounded-full flex flex-col justify-end rounded-full">
           <Image
               src="/loading.png"
               alt="Yaatrip"
@@ -54,28 +76,10 @@ export default function Home() {
               />
         </div>
       </main>
-      <div className="flex text-2xl items-center justify-center text-primary">
-            <p>Coming Soon...</p>
+      <div className="px-6">
+            <p className="text-[#238858] font-semibold text-3xl">Coming Soon...</p>
       </div>
-
-
-      <footer className="flex gap-6 flex-wrap items-center justify-center">
-      <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to Tulsi Tech Labs →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
