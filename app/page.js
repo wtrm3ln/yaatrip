31413ac75a0505d0b66dc8a36915886c6e0dcee0
wtrm3ln <142867065+wtrm3ln@users.noreply.github.com>
2024-09-11 12:48:19 +0530

    h

--- app/page.js
+++ app/page.js
@@ -4,12 +4,32 @@ export default function Home() {
   return (
     <div className="h-screen grid justify-center items-center">
 
-      <div className="fixed rounded-full bg-primary -top-[70vh] md:top-0 md:-left-[70vh] h-[90vh] w-[90vh] flex items-center justify-center rounded-full">
+      <div className="hidden md:flex fixed rounded-full bg-primary bottom-0 md:top-0 md:-left-[70vh] h-[90vh] w-[90vh] flex items-center justify-center rounded-full">
           <div className="rounded-full w-[90%] h-[90%] border-2 border-light flex items-center justify-center">
           <div className="rounded-full w-[97%] h-[97%] border-dashed border-2 border-light">
             
             </div>
           </div>
+        </div>
+
+        <div className="fixed top-0 right-0 p-4 flex gap-6 flex-wrap items-center justify-center">
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              info@yaatrip.com
+            </a>
+
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              +91 9499116400 
+            </a>
         </div>
 
       <main className="mx-auto my-36 md:my-auto md:flex py-14 px-6 md:pl-48">
@@ -24,9 +44,6 @@ export default function Home() {
         
 
         <div className="-ml-10 relative rounded-full flex items-center justify-center rounded-full">
-          <div className="z-10 absolute inset-0 flex items-center justify-center text-primary">
-            <p>Coming Soon...</p>
-          </div>
           <Image
               src="/loading.png"
               alt="Yaatrip"
@@ -37,6 +54,9 @@ export default function Home() {
               />
         </div>
       </main>
+      <div className="flex text-2xl items-center justify-center text-primary">
+            <p>Coming Soon...</p>
+      </div>
 
 
       <footer className="flex gap-6 flex-wrap items-center justify-center">
@@ -54,23 +74,6 @@ export default function Home() {
             height={16}
           />
           Go to Tulsi Tech Labs →
-        </a>
-        <a
-          className="flex items-center gap-2 text-gray-600 hover:underline hover:underline-offset-4"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          info@yaatrip.com
-        </a>
-
-        <a
-          className="flex items-center text-gray-600 gap-2 hover:underline hover:underline-offset-4"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          +91 9499116400 
         </a>
       </footer>
     </div>
